c94276c8bb36158510f96eadf2f17fe82302745c
anilkumar2870 <duddugolaanilkumar2870@gmail.com>
2026-03-26 12:05:26 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,32 @@
 import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [users, setUsers] = useState<any[]>([]);
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
+    fetch("http://13.201.35.42/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.error(err));
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+    <div style={{ padding: "20px" }}>
+      <h1>Users from EC2 API</h1>
+
+      {users.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        users.map((user) => (
+          <div key={user.id}>
+            <p><b>ID:</b> {user.id}</p>
+            <p><b>Name:</b> {user.name}</p>
+            <p><b>Email:</b> {user.email}</p>
+            <hr />
+          </div>
+        ))
+      )}
+    </div>
   );
 }
 
